7a88a89a6099b94d5a1ae4dc99eb4a84e0c60e1a
AratioD <antti.heroja@iki.fi>
2018-12-26 17:20:32 +0200

    initial version of front end fill up form

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import FillForm from './FillForm';
 import Header from './Header';
 import Footer from './Footer';
 import Content from './Content';
 
+
 class MainPage extends Component {
 
     render() {
+
         return <div>
+            
+            <FillForm />
             <Header />
             <Content />
             <Footer />
@@ -14,4 +19,4 @@ class MainPage extends Component {
     }
 }
 
-export default MainPage;
+export default MainPage; 
