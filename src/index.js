ed4eea42b0505c3d0535cb45bef2ee41d7caa403
AratioD <antti.heroja@iki.fi>
2019-02-19 22:13:31 +0200

    started implementing redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
+import rootReducer from './reducers'
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 
+const store = createStore(rootReducer)
+
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>,
+    </Provider>,
     document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
