f88c9cc210691c69d7ec0a004ffa27200efbd7e7
AratioD <antti.heroja@iki.fi>
2019-03-04 20:10:35 +0200

    modified TableReservation component to work with redux form state

--- src/components/fillform/TableReservation.jsx
+++ src/components/fillform/TableReservation.jsx
@@ -6,43 +6,75 @@ let TableReservation = props => {
     const { handleSubmit } = props
     return (
         <div>
-            < br ></br >
-            <br></br>
+            <br />
+            <br />
+            <br />
             <div class="ui black massive circular label"><div class="textcolor">Pöytäpaikkavaraus</div></div>
-            <br></br>
-            <br></br>
+            <br />
+            <br />
 
+            <center>
+                <div class="ui black compact message">
+                    <div class="textcolor">
+                        Pöydän koko on 85 x 75 cm ja vieressä on pehmustettu tuoli!
+                <p />Yksi pistorasia ja yksi nettipistoke per pöytä.
+                <p />Tuo oma jatkojohto ja oma nettipiuha (varajohtoja löytyy kyllä).
+                </div>
+                </div>
+            </center>
 
-
-            <center><div class="ui black message six wide field"><div class="textcolor">
-                Pöydän koko on 85 x 75 cm ja vieressä on pehmustettu tuoli!
-                Yksi pistorasia ja yksi nettipistoke per pöytä.
-              Tuo oma jatkojohto ja oma nettipiuha (varajohtoja löytyy kyllä).
-</div></div></center>
-
-            <br></br>
-
-
+            <br />
+            <br />
 
             <div class="ui one column centered grid">
                 <div class="five column centered row">
                     <div class="column ui checkbox">
-                        <input type="checkbox" name="example" />
+                        <Field
+                            name="pc"
+                            id="pc"
+                            component="input"
+                            type="checkbox"
+                        />
                         <label><div class="ui black circular label"><div class="textcolor">PC pelikone/läppäri</div></div></label>
                     </div>
                 </div>
 
+
                 <div class="five column centered row">
                     <div class="column ui checkbox">
-                        <input type="checkbox" name="example" />
+                        <Field
+                            name="consol"
+                            id="consol"
+                            component="input"
+                            type="checkbox"
+                        />
                         <label><div class="ui black circular label"><div class="textcolor">Konsoli</div></div></label>
+                    </div>
+                </div>
+
+
+                <div class="five column centered row">
+                    <div class="column ui checkbox">
+                        <Field
+                            name="hangAround"
+                            id="hangAround"
+                            component="input"
+                            type="checkbox"
+                        />
+                        <label><div class="ui black circular label"><div class="textcolor">Hengailu</div></div></label>
                     </div>
                 </div>
 
                 <div class="five column centered row">
                     <div class="column ui checkbox">
-                        <input type="checkbox" name="example" />
-                        <label><div class="ui black circular label"><div class="textcolor">Hengailu</div></div></label>
+                        <Field
+                            name="hangAround"
+                            id="hangAround"
+                            component="input"
+                            type="checkbox"
+                        />
+                        <label><div class="ui black circular label"><div class="textcolor">Muu</div></div></label>
+                        <div class="ui input">  <Field name="hangAroundMisc" type="text" component="input" placeholder="Mite muuta teet :-)" /></div>
                     </div>
                 </div>
 
@@ -58,8 +90,7 @@ let TableReservation = props => {
                     </div>
                 </div>
             </div>
-
-        </div>
+        </div >
     )
 }
 
