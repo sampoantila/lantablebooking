df7d80c622cf2ddc64f9103dea1c4a9fac241cb7
AratioD <antti.heroja@iki.fi>
2019-03-08 14:58:35 +0200

    css scaling bug fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './css/App.css';
+// import './css/App.css'; //outcommented because of constraints which violates current style sheet
 import MainPage from './components/MainPage';
 import { Route } from 'react-router-dom';
 import AdminPage from './components/AdminPage';
